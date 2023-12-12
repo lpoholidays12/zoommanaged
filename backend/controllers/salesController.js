442a1f544b116c64cf201f3905eb91ba07ddad84
ChinmayLokhande <chinmaylokhande.py@gmail.com>
2023-12-12 05:41:18 +0530

    updated database and made changes in packages schema

--- backend/controllers/salesController.js
+++ backend/controllers/salesController.js
@@ -2,20 +2,19 @@ import db from "./dbController.js"
 
 
 const getAllPackages = async (req, res) => {
-    var packageData = await db.query(`SELECT * from packages`);
+  var packageLists = []
+  var packageData = await db.query(`SELECT * from packages`);
+  for(let i = 0; i < packageData[0].length; i++){
+    packageData[0][i]['PackageList'] =[];
+    var subPackageData = await db.query(`SELECT * from sub_package WHERE sub_package.package_id =`+packageData[0][i]['package_id'])
+    for(let j = 0; j<subPackageData[0].length;j++){
+      packageLists.push(subPackageData[0][j])
+    }
+    packageData[0][i]['PackageList'].push(packageLists);
+    packageLists=[]
+  }
 
-  const packages = packageData[0].reduce((result, packageItem) => {
-    const packageType = packageItem.package_type;
-
-    result[packageType] = result[packageType] || [];
-
-    // Push the package to the array
-    result[packageType].push(packageItem);
-
-    return result;
-  }, {});
-
-  res.json({ packages })
+  res.json({ packages:packageData[0] })
 }
 
 export {getAllPackages}
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,29 +1,29 @@
-import  db from './dbController.js';
+import db from './dbController.js';
 import md5 from 'md5';
 import { createJwt } from '../middlewares/auth.js';
 
 const loginUser = async (req, res) => {
-    var userid = req.body.userid;
+  var userid = req.body.userid;
   var password = md5(req.body.password);
 
   var existinguser = await db.query("Select employee_id, password, department_id from employees where name='" + userid + "'")
 
   if (existinguser[0][0] == undefined) {
-    return res.json({status:"404",message:"User not found"})
+    return res.json({ status: "404", message: "User not found" })
   }
   else {
     if (password === existinguser[0][0].password) {
-      
+
       var department = await db.query("Select name from departments where department_id='" + existinguser[0][0].department_id + "'")
       const user = {
         employee_id: existinguser[0][0].employee_id,
         name: userid,
-        department : department[0][0].name,
+        department: department[0][0].name,
       }
       const token = createJwt(user);
       res.json({ Login: true, token: token })
     } else {
-      res.json({Value: true, status:"401",message:"Unauthorize Access: Password didnt match"});
+      res.json({ Value: true, status: "401", message: "Unauthorize Access: Password didnt match" });
     }
   }
 }
@@ -38,7 +38,7 @@ const loginUser = async (req, res) => {
 //     }catch(err){ 
 //         res.status(400).json({error:err.message})
 //     }
-    
+
 // }
 
-export {loginUser}
+export { loginUser }
